17bbf0fc12e4fb6d3f5d0b9d80118bffe9b9e127
Martim Afonso <martimafonso185@gmail.com>
2025-09-12 16:01:42 +0100

    added debug flag to get_all_versions

--- scholarly/_scholarly.py
+++ scholarly/_scholarly.py
@@ -238,10 +238,10 @@ class _Scholarly:
             object = publication_parser.fill(object)
         return object
 
-    def get_all_versions(self, object: Publication)->list:
+    def get_all_versions(self, object: Publication, debug: bool = False)->list:
         """Get all versions of a publication"""
         publication_parser = PublicationParser(self.__nav)
-        return publication_parser.get_all_versions(object)
+        return publication_parser.get_all_versions(object, debug)
 
     def bibtex(self, object: Publication)->str:
         """Returns a bibtex entry for a publication that has either Scholar source
